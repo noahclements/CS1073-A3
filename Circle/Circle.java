26427dc5b19a5afc3561ed1a562a49ddb2979ac1
Noah Clements <noahnoah900@gmail.com>
2018-10-10 16:03:34 -0300

    added comments

--- Circle/Circle.java
+++ Circle/Circle.java
@@ -68,46 +68,67 @@ public class Circle{
 		radius = radiusIn;
 	}
 
+	/**
+		determines the closest distance from one circle to another circle.
+		@param input Circle constructor method 
+		@return returns the closest distance from one circle to another
+	*/
 	public double distanceTo(Circle input) {
 		double distanceTo;
-		if(contains(input) == true) {
+		if(contains(input) == true) {			// if the circle contains another circle within it, distanceTo is 0
 			distanceTo = 0;
-		} else if(intersects(input) == true) {
+		} else if(intersects(input) == true) {	// if the circle intersects with another circle, distanceTo is 0
 			distanceTo = 0;
 		} else { 
-			distanceTo = center.distance(input.getCenter()) - radius - input.getRadius();		
+			distanceTo = center.distance(input.getCenter()) - radius - input.getRadius();	
+				// if both are false, distanceTo is the distance from both center points minus the radius of both circles
 		}
-		return distanceTo;
+		return distanceTo; 
 	}	 
 
-
+	/**
+		checks to see if one circle intercepts with another circle 
+		@param input Circle constructor method
+		@return returns whether a circle intercepts with another circle
+	*/
 	public boolean intersects(Circle input) {
 		boolean intersects;
-		if(center.distance(input.getCenter()) < radius + input.getRadius()) {
-			intersects = true;
+		if(center.distance(input.getCenter()) < radius + input.getRadius()) {	
+		// if the distance between two center points of circles is LESS than both of the circle's radius' combined...
+			intersects = true; // the boolean "intersects" would equal true 
 		} else {
-			intersects = false;
+			intersects = false; // if the distance is MORE than both of the circle's radius' combined, the boolean would equal false
 		}
 		return intersects;
 	}
 	
-	
+	/**
+		Checks to see if a circle contains another circle within it
+		@param other Circle constructor method
+		@return returns whether a circle contains another circle within it
+	*/
 	public boolean contains(Circle other) {
 		//double circleDistance = radius - other.getRadius();
 		boolean containsCircle;
-		if(center.distance(other.getCenter()) <= (radius - other.getRadius())) {
+		if(center.distance(other.getCenter()) <= (radius - other.getRadius())) {	
+		// if the distance between two center points of circles is less than or equal to the radius of the first circle 
+		// minus the second circles radius, the boolean "containsCircle" will be true
 			containsCircle = true;
 		} else {
-			containsCircle = false;
+			containsCircle = false;	
 		}
 		return containsCircle;
 	}
 
-
+	/**
+		Checks to see if a circle contains another circle within it
+		@param input CartesianPoint constructor method
+	*/
 	public boolean contains(CartesianPoint input) {
 		//radius minus the distance from center to point 
 		boolean containsCartesian;
-		if(center.distance(input) < radius) {
+		if(center.distance(input) < radius) { 
+		// if the distance between two circles is less than the radius of the first circle, "containsCartesian" will be true
 			containsCartesian = true;
 		} else {
 			containsCartesian = false;
