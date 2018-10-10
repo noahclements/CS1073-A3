51a7462d55236e83abeb4001ba243f82cb7b84ed
Noah Clements <noahnoah900@gmail.com>
2018-10-10 15:17:33 -0300

    removed old code

--- Scratch/Scratch.java
+++ Scratch/Scratch.java
@@ -26,36 +26,5 @@ public class Scratch {
 }
 
 
-
-
-
-/*
-
-		if(input == 1) {
-			System.out.println("Dont bother");
-		} else {
-			System.out.println("Is it expensive?");
-		}
-			input = scan.nextInt();
-		if(input == 1) {
-			System.out.println("Definitley scratch it!");
-		} else {
-			System.out.println("Have I been yelled at for scratching it before?");
-		}
-			input = scan.nextInt();
-		if(input == 1) {
-			System.out.println("Definitley scratch it!");
-		} else {
-			System.out.println("Will Dr.Batemen be angry if I scratch it?");
-		}
-			input = scan.nextInt();
-		if(input == 1) {
-			System.out.println("Definitley scratch it!");
-		} else {
-			System.out.println("Dont bother.");
-		}
-
-*/
-
 	}
 }
