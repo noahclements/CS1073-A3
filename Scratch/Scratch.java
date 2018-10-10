699ec93e931ad323c13270b2160e424947ee7e4e
Noah Clements <noahnoah900@gmail.com>
2018-10-10 15:30:14 -0300

    added comments

--- Scratch/Scratch.java
+++ Scratch/Scratch.java
@@ -7,21 +7,21 @@ public class Scratch {
 
 		System.out.println("Am I supposed to scratch it?");
 		int input = scan.nextInt();
-		if(input == 0) {
+		if(input == 0) {							// if the user enters 0 (no), it then asks if its expensive
 			System.out.println("Is it expensive?");
 				input = scan.nextInt();
-		if(input == 0) {
+		if(input == 0) {							// if the user enters 0, it then asks if jango will be yelled at
 			System.out.println("Have I been yelled at for scratching it before?");
 				input = scan.nextInt();
-		if(input == 0) {
+		if(input == 0) {							// if the user enters 0, it then asks if dr Batemen will be angry
 			System.out.println("Will Dr.Batemen be angry if I scratch it?");
 				input = scan.nextInt();
-		if(input == 0) {
+		if(input == 0) {							// if the user enters 0, it then just says "Dont bother"
 			System.out.println("Dont bother");
 		} 
 	}
-} 
-} if(input == 1){
+} 				
+} if(input == 1){ // if the user ever enters 1 (yes) during the nested if statements, it will tell Jango to scratch it
 	System.out.println("Definitley scratch it!");
 }
 
